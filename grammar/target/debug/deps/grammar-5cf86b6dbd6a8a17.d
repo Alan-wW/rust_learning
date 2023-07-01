405e4e6452c0762a3539b72030f8d864c4069fa6
Alan_ <alanwilson@163.com>
2023-07-01 22:37:05 +0800

    init

---
+++ grammar/target/debug/deps/grammar-5cf86b6dbd6a8a17.d
@@ -0,0 +1,5 @@
+/Users/alan/Documents/code/rust_learning/grammar/target/debug/deps/grammar-5cf86b6dbd6a8a17.rmeta: src/main.rs
+
+/Users/alan/Documents/code/rust_learning/grammar/target/debug/deps/grammar-5cf86b6dbd6a8a17.d: src/main.rs
+
+src/main.rs:
---
+++ grammar/target/debug/deps/grammar-bdde789a846e1e2b.d
@@ -0,0 +1,5 @@
+/Users/alan/Documents/code/rust_learning/grammar/target/debug/deps/grammar-bdde789a846e1e2b.rmeta: src/main.rs
+
+/Users/alan/Documents/code/rust_learning/grammar/target/debug/deps/grammar-bdde789a846e1e2b.d: src/main.rs
+
+src/main.rs:
